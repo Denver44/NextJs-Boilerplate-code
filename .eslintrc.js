11ae591d59f760c5ec009ea9dd2c652afc7b78b4
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-30 18:45:38 +0530

    husky eslint and prettier configure done

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'google', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'require-jsdoc': 0,
+
+    // We will use TypeScript's types for component props instead
+    'react/prop-types': 'off',
+    'spaced-comment': [
+      'error',
+      'always',
+      {
+        line: {
+          markers: ['/'],
+        },
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'latest',
+    },
+  },
+};
