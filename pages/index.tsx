331ddf2b664d3b56a057c46061c81a7c1fa83a88
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-30 18:24:16 +0530

    Scss added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Contact from './Contact';
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
-`
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <>
+      <Title>My page</Title>
+      <Contact />
+    </>
+  );
 }
