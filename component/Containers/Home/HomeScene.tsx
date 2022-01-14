b2beca3e543b0e36961ffc9f86978182a20af5b6
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-14 19:43:32 +0530

    message

---
+++ component/Containers/Home/HomeScene.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const HomeScene = () => {
+  return <>Home</>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,3 @@
-import React from 'react';
-import styled from 'styled-components';
-import Contact from './Contact';
+import HomeContainer from '../component/Containers/Home/HomeContainer';
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`;
-
-export default function Home() {
-  return (
-    <>
-      <Title>My page</Title>
-      <Contact />
-    </>
-  );
-}
+export default HomeContainer;
